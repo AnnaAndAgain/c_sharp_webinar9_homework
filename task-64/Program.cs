0d258291cbcd62c9cd190fbedeaa8dd4bd741a42
Anna <a.s.markovskaya@gmail.com>
2023-07-21 11:50:11 +0300

    добавила на всякий случай удаленную из списка третью задачу

---
+++ task-64/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Выводим список от N до 1
+Используем рекурсию
+*/
+
+
+int ReadInt (string text)
+{
+    Console.WriteLine(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void Numbers (int N)
+{
+    if (N == 0) return;
+    Console.Write(N + " ");
+    Numbers(N-1);
+}
+
+void Main()
+{
+    int N = ReadInt("Введите число");
+    Numbers(N);
+}
+
+Main();
+
+

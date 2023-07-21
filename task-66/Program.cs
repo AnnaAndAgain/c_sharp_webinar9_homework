14b9f1d4305876475aed8e16f2b9454656b6b8db
Anna <a.s.markovskaya@gmail.com>
2023-07-21 10:56:52 +0300

    обе задачи

---
+++ task-66/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+
+*/
+
+
+int ReadInt (string text)
+{
+    Console.WriteLine(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int SumNumbers (int M, int N)
+{
+    if (M > N) return 0;
+    return M + SumNumbers(M+1, N);
+}
+
+bool CheckInputNumbers (int M, int N)
+{
+    if (M < N) return true;
+    else return false;
+}
+
+void Main()
+{
+    int M = ReadInt("Введите первое число");
+    int N = ReadInt("Введите второе число");
+    if (CheckInputNumbers(M, N))
+    {
+        Console.WriteLine(SumNumbers(M, N));
+    }
+    else 
+    {
+        Console.WriteLine(SumNumbers(N, M));
+    }
+}
+
+Main();
+
+
---
+++ task-68/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана 
+с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+
+*/
+
+int ReadInt (string text)
+{
+    Console.WriteLine(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int Ack (int M, int N)
+{
+    if (M == 0) return N+1;
+    else if (N == 0) return Ack(M-1, 1);
+    else return Ack(M-1, Ack(M, N-1));
+}
+
+bool CheckInputNumbers (int M, int N)
+{
+    if (M < 0 || N<0) return false;
+    else return true;
+}
+
+void Main()
+{
+    int M = ReadInt("Введите первое число (положительное или 0)");
+    int N = ReadInt("Введите второе число (положительное или 0)");
+    if (CheckInputNumbers(M, N)) Console.WriteLine(Ack(M, N));
+    else System.Console.WriteLine("Отрицательные числа нам не подойдут =(");
+}
+
+Main();
